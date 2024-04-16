ffe093a19ef6c6ea9dd80e08936d73bf9d869d33
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2024-04-16 14:23:22 +0200

    ordner überarbeitet

---
+++ src/main/java/com/WebTech/WebTechSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebTech_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechSpringApplication.class, args);
+	}
+
+}
