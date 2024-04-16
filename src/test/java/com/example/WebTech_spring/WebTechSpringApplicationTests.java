865efae72c6e68b2211ac318e49e9d50dfe739c5
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2024-04-16 13:09:04 +0200

    spring_new_project

---
+++ src/test/java/com/example/WebTech_spring/WebTechSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebTech_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
