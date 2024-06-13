277b55ec101a68730d07719510a7cb74726f100f
Hüseyin Onat <h-onat@web.de>
2024-06-13 23:19:39 +0200

    serviceKlasse und repositoryInterface update

--- src/main/java/com/WebTech/KleidungController.java
+++ src/main/java/com/WebTech/KleidungController.java
@@ -1,5 +1,6 @@
 package com.WebTech;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import java.util.List;
 
 @RestController
 public class KleidungController {
+
+    @Autowired
+    KleidungService service;
 
     @GetMapping("/api/suche")
     @CrossOrigin
--- src/main/java/com/WebTech/KleidungRepository.java
+++ src/main/java/com/WebTech/KleidungRepository.java
@@ -3,4 +3,5 @@ package com.WebTech;
 import org.springframework.data.repository.CrudRepository;
 
 public interface KleidungRepository extends CrudRepository<Kleidung, Long> {
+    //Datentyp Long?
 }
